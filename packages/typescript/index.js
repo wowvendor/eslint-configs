7d6d00c5b1ac947c69f1fb790e21a71a36c1c884
weyheyhey <iweyheyhey@gmail.com>
2019-10-17 21:02:13 +0300

    fix(typescript): invalid array-type rule value

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -48,7 +48,7 @@ module.exports = {
 
         "@typescript-eslint/no-namespace": "error",
         "@typescript-eslint/no-explicit-any": "error",
-        "@typescript-eslint/array-type": ["error", "array-simple"],
+        "@typescript-eslint/array-type": ["error", { default: "array-simple" }],
         "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
         "@typescript-eslint/member-ordering": [
           "error",
